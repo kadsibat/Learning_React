d91b940ddc0556787cb45ba21b42dc1e79c7b43e
Batuhan <yesiltas_sibel@hotmail.com>
2022-03-29 14:47:29 +0300

    6

---
+++ rc06_styling_2/styling-2/src/components/materialUi/MaterialUi.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Button from "./button/Button"
+
+const MaterialUi = () => {
+  return (
+    
+    <div>
+
+    <Button/>
+
+    </div>
+  )
+}
+
+export default MaterialUi
---
+++ rc06_styling_2/styling-2/src/components/materialUi/button/Button.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+
+export default function BasicButtons() {
+  return (
+    <>
+     
+      <Button variant="contained">Contained</Button>
+
+      </>
+  );
+}
