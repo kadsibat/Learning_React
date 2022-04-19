597bfcc0f46f48e92b8285d46acf7de6c32f57b2
Batuhan <yesiltas_sibel@hotmail.com>
2022-04-19 16:57:27 +0300

    redux

---
+++ rc13-redux2_starter/src/redux/store/index.jsx
@@ -0,0 +1,12 @@
+import { createStore } from "redux";
+import rootReducer from "../reducers"
+import { composeWithDevTools } from '@redux-devtools/extension';
+
+const store =createStore(rootReducer,
+  
+  composeWithDevTools(
+    // applyMiddleware(...middleware)
+    // other store enhancers if any
+  )
+);
+export default store;
